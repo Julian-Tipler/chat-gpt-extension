211c08c1ce5a16abf21d42e16c824afbef81b4db
Julian <tipler.julian@gmail.com>
2023-10-29 13:46:49 -0500

    removed console logs

--- content/content.js
+++ content/content.js
@@ -1,8 +1,5 @@
-console.log("HELLO");
 // Listens for changeText action and changes the text in the target website
 chrome.runtime.onMessage.addListener(function (request) {
-  console.log("received message");
-  console.log(request.action);
   if (request.action === "changeText") {
     const searchBar = document.getElementById("prompt-textarea");
     console.log(searchBar);
