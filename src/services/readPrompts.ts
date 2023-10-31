e5876f24d29e6d4937dea0e820328b6145f6da84
Julian <tipler.julian@gmail.com>
2023-10-31 10:36:57 -0500

    started autocomplete but unrelated

--- src/services/readPrompts.ts
+++ src/services/readPrompts.ts
@@ -1,9 +1,15 @@
 import { Prompt } from "../App";
 
-export const readPrompts = async () => {
-  const prompts: Prompt[] = [];
-  chrome.storage.sync.get(null, function(payload: any) {
-    if (payload.prompts) prompts.push(...payload.prompts);
+export const readPrompts = async (): Promise<Prompt[]> => {
+  return new Promise((resolve, reject) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    chrome.storage.sync.get({ prompts: [] }, function(payload: any) {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        const prompts = payload.prompts || [];
+        resolve(prompts as Prompt[]);
+      }
+    });
   });
-  return prompts as Prompt[];
 };
