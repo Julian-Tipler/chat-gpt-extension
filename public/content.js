4af70699a7c80c3f38d44be1563fc0ff4173364c
Julian <tipler.julian@gmail.com>
2023-11-01 16:48:50 -0500

    hello world autocomplete working

--- public/content.js
+++ public/content.js
@@ -1,6 +1,90 @@
 console.log("content script running !!!");
 
-// Prompts
+// AUTOCOMPLETE
+// UI variables
+const textArea =
+  document.querySelector('input[type="text"]') ||
+  document.querySelector("textarea");
+
+const textAreaContainer = textArea.parentNode;
+textAreaContainer.classList.add("text-area-container");
+
+const superTextArea = document.createElement("div");
+superTextArea.classList.add("super-text-area");
+superTextArea.setAttribute("contenteditable", true);
+textAreaContainer.appendChild(superTextArea);
+
+let ghostText = "";
+
+// Listeners
+textArea.addEventListener("input", () => {
+  lastInputTime = Date.now();
+  superTextArea.textContent = textArea.value;
+  const ghostTextContainer = document.createElement("span");
+  ghostTextContainer.id = "ghost-text";
+  ghostText = "";
+  ghostTextContainer.textContent = ghostText;
+  superTextArea.appendChild(ghostTextContainer);
+  console.log(superTextArea);
+});
+let lastInputTime = Date.now();
+const DELAY_AFTER_TYPING = 2000;
+setInterval(maybeAddAutocomplete, 1000);
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Tab") {
+    console.log("tab pressed");
+    event.preventDefault();
+    textArea.value += ghostText;
+    textArea.dispatchEvent(new Event("input", { bubbles: true }));
+    textArea.focus();
+  }
+});
+
+//Functions
+async function maybeAddAutocomplete() {
+  // Has it been enough time since last user input and is the text itself long enough to warrant a fetch?
+  if (
+    Date.now() - lastInputTime >= DELAY_AFTER_TYPING &&
+    textArea.value.length > 10
+  ) {
+    const autocomplete = await fetchAutocomplete();
+    addGhostText(autocomplete);
+  }
+}
+function addGhostText(autocomplete) {
+  const existingGhostTextContainer = document.getElementById("ghost-text");
+  ghostText = autocomplete;
+  existingGhostTextContainer.textContent = ghostText;
+}
+
+function fetchAutocomplete() {
+  console.log("fetching autocomplete");
+  // Fetch a recommendation from your ChatGPT API
+  const content = textArea.textContent;
+  const apiUrl =
+    "http://localhost:54321/functions/v1/autocomplete?userId=80c39e74-7767-44a4-b6cf-2b2baa040a71";
+  const accessToken =
+    ***REMOVED***;
+  return fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({ content }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data.autocomplete", data.autocomplete);
+      return data.autocomplete;
+    })
+    .catch((error) => {
+      console.error("error", error);
+    });
+}
+
+// PROMPTS
 chrome.runtime.onMessage.addListener(function(request) {
   switch (request.action) {
     case "changeText":
@@ -14,98 +98,4 @@ function changeText(text) {
   if (searchBar) {
     searchBar.value += text;
   }
-}
-
-// Autocomplete
-// Global variables
-const textArea =
-  document.querySelector('input[type="text"]') ||
-  document.querySelector("textarea");
-const textAreaContainer = textArea.parentNode;
-let superTextInput;
-let lastInputTime = Date.now();
-const recommendationDelay = 1500;
-
-//Listeners
-document.addEventListener("input", () => {
-  lastInputTime = Date.now();
-});
-document.addEventListener("keydown", function(event) {
-  if (event.key === "Tab") {
-    event.preventDefault();
-    insertAutocompleteText("hello text");
-  }
-});
-
-//Functions
-function fetchAutocomplete() {
-  if (Date.now() - lastInputTime >= recommendationDelay) {
-    // Fetch a recommendation from your ChatGPT API
-    const content =
-      "There once was a rubber duck named 'Quack'. He lived in a very nice";
-    const apiUrl =
-      "http://localhost:54321/functions/v1/autocomplete?userId=<redacted>";
-    const accessToken = "<redacted>";
-    fetch(apiUrl, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      },
-      body: JSON.stringify({ content }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("data", data);
-        const autocomplete = data.autocomplete; // Extract the recommendation from the API response
-        if (autocomplete) {
-          // Insert the recommendation into the text input or textarea
-          addAutocompleteText(autocomplete);
-        }
-      })
-      .catch((error) => {
-        console.error("error", error);
-      });
-  }
-}
-setInterval(() => {}, 1000000);
-
-function modifySuperTextInput() {
-  // Create super text input
-  textAreaContainer.classList.add("text-area-container");
-  superTextInput = document.createElement("div");
-  superTextInput.classList.add("super-text-input");
-  superTextInput.setAttribute("contenteditable", true);
-  superTextInput.addEventListener("input", () => {});
-  textAreaContainer.appendChild(superTextInput);
-
-  // Makes the value of textArea equal to superTextInput (underneath) when typing
-  superTextInput.addEventListener("input", () => {
-    textArea.value = superTextInput.value;
-    textArea.dispatchEvent(new Event("input", { bubbles: true }));
-  });
-}
-modifySuperTextInput();
-
-function addAutocompleteText(autocomplete) {
-  if (textArea) {
-    textArea.focus();
-
-    const ghostText = document.createElement("span");
-    ghostText.classList.add("ghost-text");
-    ghostText.textContent += autocomplete;
-    superTextInput.appendChild(ghostText);
-
-    console.log(superTextInput);
-    textArea.dispatchEvent(new Event("input", { bubbles: true }));
-    lastInputTime = Date.now();
-  }
-}
-// setInterval(() => {
-//   addAutocompleteText("hello");
-// }, 5000);
-
-function insertAutocompleteText(autocomplete) {
-  textArea.textContent += autocomplete;
-  textArea.dispatchEvent(new Event("input", { bubbles: true }));
 }
