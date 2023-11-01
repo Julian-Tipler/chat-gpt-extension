d492cd199823f0a8378aa84fefea5587df0d9af3
Julian <tipler.julian@gmail.com>
2023-11-01 14:23:50 -0500

    about to delete eslint file

--- public/content.js
+++ public/content.js
@@ -1,4 +1,5 @@
-console.log("content script running 🚀");
+console.log("content script running !!!");
+sdf = "hello"
 
 // Prompts
 chrome.runtime.onMessage.addListener(function(request) {
@@ -16,22 +17,36 @@ function changeText(text) {
   }
 }
 
-//Autocomplete
+// Autocomplete
+// Global variables
+const textArea =
+  document.querySelector('input[type="text"]') ||
+  document.querySelector("textarea");
+const textAreaContainer = textArea.parentNode;
+let superTextInput;
 let lastInputTime = Date.now();
-const recommendationDelay = 2000;
+const recommendationDelay = 1500;
+
+//Listeners
 document.addEventListener("input", () => {
   lastInputTime = Date.now();
 });
 
-const content =
-  "There once was a rubber duck named 'Quack'. He lived in a very nice";
-const apiUrl =
-  "http://localhost:54321/functions/v1/autocomplete?userId=<redacted>";
-const accessToken = "<redacted>";
-
-setInterval(() => {
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Tab") {
+    event.preventDefault();
+    insertAutocompleteText("hello text");
+  }
+});
+//Functions
+function fetchAutocomplete() {
   if (Date.now() - lastInputTime >= recommendationDelay) {
     // Fetch a recommendation from your ChatGPT API
+    const content =
+      "There once was a rubber duck named 'Quack'. He lived in a very nice";
+    const apiUrl =
+      "http://localhost:54321/functions/v1/autocomplete?userId=<redacted>";
+    const accessToken = "<redacted>";
     fetch(apiUrl, {
       method: "POST",
       headers: {
@@ -46,34 +61,53 @@ setInterval(() => {
         const autocomplete = data.autocomplete; // Extract the recommendation from the API response
         if (autocomplete) {
           // Insert the recommendation into the text input or textarea
-          createAutocompleteText(autocomplete);
+          addAutocompleteText(autocomplete);
         }
       })
       .catch((error) => {
         console.error("error", error);
       });
   }
-}, 1000000);
+}
 
-function createAutocompleteText(autocomplete) {
-  const textArea =
-    document.querySelector('input[type="text"]') ||
-    document.querySelector("textarea");
+setInterval(() => {}, 1000000);
+
+// Already present components
+
+function modifySuperTextInput() {
+  // Create super text input
+  textAreaContainer.classList.add("text-area-container");
+  superTextInput = document.createElement("div");
+  superTextInput.classList.add("super-text-input");
+  superTextInput.setAttribute("contenteditable", true);
+  superTextInput.addEventListener("input", () => {});
+  textAreaContainer.appendChild(superTextInput);
+
+  // Makes the value of textArea equal to superTextInput (underneath) when typing
+  superTextInput.addEventListener("input", () => {
+    textArea.value = superTextInput.value;
+    textArea.dispatchEvent(new Event("input", { bubbles: true }));
+  });
+}
+modifySuperTextInput();
+
+function addAutocompleteText(autocomplete) {
   if (textArea) {
     textArea.focus();
+
     const ghostText = document.createElement("span");
-    ghostText.style.opacity = 0.5;
+    ghostText.classList.add("ghost-text");
     ghostText.textContent += autocomplete;
-    console.log("ghostText", ghostText);
-    textArea.parentNode.appendChild(ghostText);
-    console.log("textArea", textArea);
+    superTextInput.appendChild(ghostText);
+
+    console.log(superTextInput);
     textArea.dispatchEvent(new Event("input", { bubbles: true }));
     lastInputTime = Date.now();
   }
 }
-setTimeout(() => {
-  createAutocompleteText("hello");
-}, 1000);
+// setInterval(() => {
+//   addAutocompleteText("hello");
+// }, 5000);
 
 function insertAutocompleteText(autocomplete) {
   const textArea =
@@ -82,10 +116,3 @@ function insertAutocompleteText(autocomplete) {
   textArea.textContent += autocomplete;
   textArea.dispatchEvent(new Event("input", { bubbles: true }));
 }
-
-document.addEventListener("keydown", function(event) {
-  if (event.key === "Tab") {
-    event.preventDefault();
-    insertAutocompleteText("hello text");
-  }
-});
--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ export type Prompt = {
 };
 
 function App() {
+  const unused = ""
   return (
     <div className="main-container">
       <Title />
