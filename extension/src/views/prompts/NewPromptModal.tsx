030ee67212603692ad10f333817b5a96f9705d57
Julian <tipler.julian@gmail.com>
2023-11-13 12:16:29 -0600

    reorganized new prompt into a modal

---
+++ extension/src/views/prompts/NewPromptModal.tsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import { usePrompts } from "../../contexts/PromptsContext";
+import { Box } from "@chakra-ui/layout";
+import { FormLabel } from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { Textarea } from "@chakra-ui/textarea";
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/modal";
+
+export type PromptFormState = {
+  name: string;
+  text: string;
+};
+
+export const NewPromptModal = ({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) => {
+  const [form, setForm] = useState({
+    name: "",
+    text: "",
+  });
+
+  const { createPrompt } = usePrompts();
+
+  const handleSubmit = ({
+    e,
+    form,
+    setForm,
+  }: {
+    e: React.FormEvent<HTMLFormElement>;
+    form: PromptFormState;
+    setForm: React.Dispatch<React.SetStateAction<PromptFormState>>;
+  }) => {
+    e.preventDefault();
+    createPrompt(form);
+    setForm({ name: "", text: "" });
+    onClose();
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} size={"sm"}>
+      <ModalOverlay />
+      <ModalContent
+        borderRadius={"1em"}
+        bgColor={"brand.cardBackground"}
+        padding={"1em"}
+      >
+        <ModalHeader>New Prompt</ModalHeader>
+        <ModalBody>
+          <form
+            id="prompt-form"
+            className="prompt-form"
+            onSubmit={(e) => handleSubmit({ e, form, setForm })}
+          >
+            <Box id="prompt-form-name-container">
+              <FormLabel htmlFor="prompt-form-name">Name:</FormLabel>
+              <Input
+                size="sm"
+                backgroundColor={"brand.background"}
+                border="none"
+                type="text"
+                id="prompt-form-name"
+                maxLength={30}
+                value={form.name}
+                onChange={(e) => setForm({ ...form, name: e.target.value })}
+                required
+              />
+            </Box>
+            <br />
+            <FormLabel htmlFor="prompt-form-text">Text:</FormLabel>
+            <Textarea
+              backgroundColor={"brand.background"}
+              border="none"
+              id="prompt-form-text"
+              required
+              value={form.text}
+              onChange={(e) => setForm({ ...form, text: e.target.value })}
+            />
+            <br />
+
+            <Input
+              className="clickable-button"
+              id="prompt-form-submit-button"
+              type="submit"
+              value="Add Prompt"
+              bgColor={"brand.primary"}
+              border={"none"}
+              _hover={{
+                backgroundColor: "brand.primaryHover",
+                cursor: "pointer",
+              }}
+            />
+          </form>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
--- extension/src/views/prompts/PromptsPage.tsx
+++ extension/src/views/prompts/PromptsPage.tsx
@@ -1,8 +1,7 @@
 //
 import { Title } from "../../components/Title";
-import { FormContainer } from "../../components/Form";
 import { Prompts } from "./Prompts";
-import { Box, Flex } from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/layout";
 
 export type Prompt = {
   id: string;
@@ -17,35 +16,13 @@ export const PromptsPage = () => {
       className="main-container"
       justifyContent={"center"}
       padding={"20px"}
-      backgroundColor={"#444654"}
-      borderRadius={"0.5em"}
+      backgroundColor={"brand.background"}
+      borderRadius={"0.5rem"}
       overflowY={"auto"}
       maxHeight={"600px"}
     >
       <Title />
-      <Flex
-        className={"prompts-container"}
-        backgroundColor={"red"}
-        flexDir={"column"}
-        flex={1}
-      >
-        <Box backgroundColor={"yellow"} overflow={"auto"}>
-          child 1
-        </Box>
-        <Box backgroundColor={"blue"}>child 2</Box>
-      </Flex>
+      <Prompts />
     </Flex>
   );
 };
-
-{
-  /* <Title />
-      <Flex flexDir={"column"} flex={1}>
-        <Box overflow={"auto"}>
-          <Prompts />
-        </Box>
-        <Box>
-          <FormContainer />
-        </Box>
-      </Flex> */
-}
