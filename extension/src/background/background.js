848cb24175340bb922ccbabdf8fbb3a434502be2
Julian <tipler.julian@gmail.com>
2023-11-30 12:12:03 -0600

    added onHeadersReceived to background script

--- extension/src/background/background.js
+++ extension/src/background/background.js
@@ -31,3 +31,25 @@ function saveWiseSessionToken(token) {
     console.log("Wise session token saved");
   });
 }
+
+chrome.webRequest.onHeadersReceived.addListener(
+  function(details) {
+    // Check if the response has a 401 status code
+    const isUnauthorized = details.statusCode === 401;
+
+    if (isUnauthorized) {
+      // Trigger a message to your content script to handle the logout
+      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: "logout" });
+      });
+    }
+
+    // Continue with the original response
+    return { responseHeaders: details.responseHeaders };
+  },
+  {
+    urls: ["<all_urls>"],
+    types: ["main_frame"],
+  },
+  ["responseHeaders"]
+);
